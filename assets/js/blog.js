4cd7c5f5161f883e5907eb2c1d4c80010e2582f6
Yugender025 <pentapuriyugender@gmail.com>
2024-11-15 17:19:27 +0530

    content updated blogs

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -10,7 +10,8 @@ const blogPosts = [
   },
   {
     title: "AWS Services: A Comprehensive Overview",
-    category: "ai",
+    category: "services",
+    link: "blog2.html",
     image: "assets/images/awsservices.WEBP",
     content:
       "Amazon Web Services (AWS) is a comprehensive and evolving cloud platform providing over 200 fully featured services from data centers globally. It serves millions of customers, inc...",
@@ -18,7 +19,8 @@ const blogPosts = [
   },
   {
     title: "How to Create an AWS Account: A Step-by-Step Guide",
-    category: "ai",
+    category: "create",
+    link: "blog3.html",
     image: "assets/images/accountaws.png",
     content:
       "Creating an AWS account is the first step toward accessing Amazon's powerful suite of cloud services, which enables..",
@@ -26,7 +28,8 @@ const blogPosts = [
   },
   {
     title: "Networking & Content Delivery",
-    category: "ai",
+    category: "services",
+    link: "blog4.html",
     image: "assets/images/network_cdn.png",
     content:
       "Amazon Web Services (AWS) offers a robust selection of networking and content delivery services to help businesses build scalable, secure, and highly available applications. These services...",
@@ -34,7 +37,8 @@ const blogPosts = [
   },
   {
     title: "How to Create an IAM user",
-    category: "ai",
+    category: "create",
+    link: "blog5.html",
     image: "assets/images/iamuser.png",
     content:
       "AWS Identity and Access Management (IAM) is an essential service that allows you to manage user access and control permissions for resources in your AWS environment. Creating individual IAM users for each person or application accessing..",
@@ -43,6 +47,7 @@ const blogPosts = [
   {
     title: "Introduction to Generative AI",
     category: "ai",
+    link: "blog6.html",
     image: "assets/images/featureblog.WEBP",
     content:
       "Lorem ipsum dolor sit amet consectetur. Fermentum nec gravida rhoncus orci feugiat faucibus mattis mollis. Ipsum in eget nibh dui sit venenatis sit amet tellus. Diam at ut velit eget..",
@@ -51,6 +56,7 @@ const blogPosts = [
   {
     title: "AWS Services in Industry",
     category: "ai",
+    link: "blog7.html",
     image: "assets/images/awsindustry.png",
     content:
       "Amazon Web Services (AWS) has become a go-to solution for businesses across various industries due to its flexibility, scalability, and wide array of services Companies worldwide leverage AWS to manage infrastructure...",
@@ -58,7 +64,8 @@ const blogPosts = [
   },
   {
     title: "Create an S3 bucket",
-    category: "ai",
+    category: "services",
+    link: "blog8.html",
     image: "assets/images/s3bucket.WEBP",
     content:
       "Amazon S3 (Simple Storage Service) is one of AWS's core services, providing scalable, secure, and durable storage for a wide range of data. From backups and data archives to hosting static websites, S3’s versatility makes it..",
